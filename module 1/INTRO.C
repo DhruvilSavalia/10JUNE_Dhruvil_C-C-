5a54bb2a06b5aa2f2adcaf91bc90d4ad73bcf384
DhruvilSavalia <109803967+DhruvilSavalia@users.noreply.github.com>
2022-07-26 18:51:17 +0530

    Add files via upload

---
+++ module 1/INTRO.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("my name is Dhruvil\n");
+printf("my birthdate is - 3/10/2003\n");
+printf("my age is -19\n");
+printf("280,chandrapark part-2,streetno-18,opp-reliance mall,150ft ringroad\n");
+getch();
+}
