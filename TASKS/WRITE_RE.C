0b617054ce7e1c46de771665b3536bd56e2f4498
DhruvilSavalia <109803967+DhruvilSavalia@users.noreply.github.com>
2022-07-28 19:23:33 +0530

    Add files via upload

--- TASKS/WRITE_RE.C
+++ TASKS/WRITE_RE.C
@@ -2,18 +2,37 @@
 #include<conio.h>
 int main()
 {
-   FILE *p;
-   char ch[20],r;
+   int no,i,j;
+   FILE *a;
+   char ch[20];
+  // char ch[20],no,i,j;
    clrscr();
-   p=fopen("hello.txt","w");
-   fprintf(p,"PATEL DHRUVIL");
-   fclose(p);
-   p=fopen("hello.txt","r");
-   printf("PATEL DHRUVIL");
-   fscanf(p,"%s",&ch);
-    fprintf(p,"%s",ch);
-   fclose(p);
-   getch();
+  // p=fopen("hello.txt","w");
+  a=fopen("Table.txt","w");
+  // fprintf(p,"PATELDHRUVIL");
+  printf("Enter number:");
+  scanf("%d",&no);
+  for(i=1;i<=10;i++)
+  {
+  j=no*i;
+    fprintf(a,"\n%d*%d=%d\n",no,i,j);
+  }
+   fclose(a);
+ //  getch();
+
+   a=fopen("Table.txt","r");
+ //printf("PATEL DHRUVIL");
+    fprintf("%d",a);
+	     for(i=1;i<=10;i++)
+	      {
+	    //j=no*i;
+	    //fprintf("\n%d",a);
+
+  fscanf(a,"%s",&ch);
+  printf("\n%s",ch);
+  }
+  fclose(a);
+  getch();
 
 }
 
