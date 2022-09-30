348f20402ba0b2ff546ac08ae50a6b36d8c4c960
DhruvilSavalia <109803967+DhruvilSavalia@users.noreply.github.com>
2022-09-30 00:47:27 -0700

    Add files via upload

---
+++ module-6/lecture.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+char a[10],b[10],c[20];
+int d;
+class lecture
+{
+   public:
+   lecture()
+   {
+      cout<<"name of the lecturer:komal raiyani"<<endl;
+      cout<<"name of the subject:English"<<endl;
+      cout<<"name of the course:12 science(B-group)"<<endl;
+      cout<<"number of the lectures in week:5"<<endl<<endl;
+
+      cout<<"name of the lecturer:Shree patel"<<endl;
+      cout<<"name of the subject:biology"<<endl;
+      cout<<"name of the course:12 science(B-group)"<<endl;
+      cout<<"number of the lectures in week:8"<<endl<<endl;
+
+      cout<<"name of the lecturer:Mitesh khunt"<<endl;
+      cout<<"name of the subject:physics"<<endl;
+      cout<<"name of the course:12 science(B-group)"<<endl;
+      cout<<"number of the lectures in week:6"<<endl<<endl;
+
+      cout<<"name of the lecturer:janvi sakhiya"<<endl;
+      cout<<"name of the subject:chemistry"<<endl;
+      cout<<"name of the course:12 science(B-group)"<<endl;
+      cout<<"number of the lectures in week:9"<<endl<<endl;
+
+      cout<<"\nname of the lecturer:nikhil dhami"<<endl;
+      cout<<"name of the subject:computer"<<endl;
+      cout<<"name of the course:12 science(B-group)"<<endl;
+      cout<<"number of the lectures in week:3"<<endl<<endl;
+   }
+};
+int main()
+{
+   lecture l;
+}     
+         
